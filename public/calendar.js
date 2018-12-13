1d52ddef7c222f45a81dd5c7f95caf24f8894c09
DavidRP1 <36389670+DavidRP1@users.noreply.github.com>
2018-12-12 20:20:46 -0500

    Update calendar.js

--- public/calendar.js
+++ public/calendar.js
@@ -1,14 +1,15 @@
 // Raby-Pepin, David & Wang, XiaoQian
-// 07-12-2018
-// Description
+// 12-12-2018
+// Script qui gere les interactions cote client avec le calendrier.
 
 ////////////////////////////////////////////////////////////////////////////////
 
 'use strict';
 
-
+// Fonction qui va chercher le nombre d'heures et de jours dans le calendrier
+// ainsi que l'emplacement de la souris de l'utilisateur et les cases du
+// calendrier dans lesquelles l'utilisateur clique
 document.addEventListener('DOMContentLoaded', function() {
-
     var calendrier = document.getElementById("calendrier");
     if(calendrier == null)
         return;
@@ -18,27 +19,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
     calendrier.addEventListener("mousedown", onClick);
     calendrier.addEventListener("mouseover", onMove);
-
 });
 
+// Fonction qui identifie les cases du calendrier dans lesquelles
+// l'utilisateur clique
 function onClick(event) {
-    // TODO
-
     /* La variable t contient l'élément HTML sur lequel le clic a été
        fait. Notez qu'il ne s'agit pas forcément d'une case <td> du
        tableau */
     var t = event.target;
+
     // Attribut id de l'élément sur lequel le clic a été fait
     var id = "";
 
-    //Fix defect: if click on element which is not a td, do not change
+    // Si l'utilisateur clique sur un element qui n'est pas une case <td>,
+    // ne rien faire
     if(t.nodeName.toLocaleLowerCase() == "td"){
         id = t.id;
         t.innerHTML = t.innerHTML == "" ? "&#10004;" : "";
     }
-        
 };
 
+// Fonction qui identifie l'emplacement de la souris de l'utilisateur
 function onMove(event) {
     var t = event.target;
     var id = "";
@@ -49,7 +51,6 @@ function onMove(event) {
 
 // Fonction qui retourne une serie de "0" et de "1" qui encode les
 // disponibilites selectionnees par les utilisateurs dans le tableau de dispos
-// ******DAVID 12/06 - TERMINEE******
 var compacterDisponibilites = function() {
     var heures = document.getElementById("calendrier").dataset.nbheures;
     var jours = document.getElementById("calendrier").dataset.nbjours;
